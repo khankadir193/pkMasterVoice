cb38c7c2d34884bb1c432c66667a3767152bbc1b
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-04-19 16:15:07 +0530

    pkMasterOfVoice has been update round1 leaderboard

--- js/google-sheets-html.js
+++ js/google-sheets-html.js
@@ -110,11 +110,11 @@ function rowsCount(item){
  
   dataList.forEach(dataCount);
   console.log("counter is"+counter);
-  if(counter>=0 && counter<=14){
+  if(counter>=0 && counter<=2){
      $('#conDiv').append('<div class="colThree"><div><a href="http://www.kktv1.com/m/?roomid='+id+'"><img src="../common/img/kk_head_avatar_men.png" id="u_img_'+id+'" alt="" class="imgClass"></a></div><div class="centerDiv"><img src="img/coin.png" class="imgsmal">'+name+'<br>ID-'+id+'</div><div class="lastDiv"><span class="dancing">'+times+'</span></div></div>');
 
   }
-  else  if(counter>14 && counter<=27){
+  else  if(counter>2 && counter<=4){
 
 
   
